5fc0753252a57ba92eaed2be2d7b0cb199b9a3d2
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-25 10:24:30 +0100

    zaibav

---
+++ DAL/UserAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UserAnswer
+    {
+        public int id { get; set; }
+        public int idUser { get; set; }
+        public int idAnswer { get; set; }
+        public DateTime DateTimes { get; set; }
+    }
+}
